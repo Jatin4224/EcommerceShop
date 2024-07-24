cdaf93e2bdfab13ceed47567793d893148cdc6eb
Jatin4224 <jaikumarsharma94130@gmail.com>
2024-07-25 00:59:09 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Products from "./Products";
+import Contact from "./Contact";
+import Single from "./Single";
+import Cart from "./Cart";
+import Error from "./Error";
 
 const App = () => {
-  return <div>Welcome to Thapa React E-Commerce Website</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/single/:id" element={<Single />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/error" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
